8b76a4ac91bc490d5dcf5152ad41f1a355adab0d
Bart S <147872443+Bartron773@users.noreply.github.com>
2026-04-15 08:31:36 -0400

    Delete drizzle directory

--- drizzle/0002_bright_patriot.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `architectural_assets` MODIFY COLUMN `userId` int NOT NULL DEFAULT 0;--> statement-breakpoint
-ALTER TABLE `node_annotations` MODIFY COLUMN `userId` int NOT NULL DEFAULT 0;
